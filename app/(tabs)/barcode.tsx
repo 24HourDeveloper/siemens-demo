4b42efdb16af73f0786dc989871f056e0bfa50f8
Michael Moore <michaelmoore@Michaels-Mac-mini.local>
2025-01-31 02:04:30 -0500

    installed react-native-camera-kit and ios pods, added Barcode page design

--- app/(tabs)/barcode.tsx
+++ app/(tabs)/barcode.tsx
@@ -1,8 +1,65 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { Camera } from 'react-native-camera-kit';
+import { ThemedText } from '@/components/ThemedText';
+import { useThemeColor } from '@/hooks/useThemeColor'
 
-export default function barcode() {
+export default function BarcodeScanner() {
+  const [scannedCodes, setScannedCodes] = useState<string[]>([]);
+  const color = useThemeColor({ light: "black", dark: 'white' }, 'text')
+
+  const onBarcodeScan = (event: { nativeEvent: { codeStringValue: string } }) => {
+    console.log('first barcode scan', event.nativeEvent);
+    const scannedValue = event.nativeEvent.codeStringValue;
+
+    if (!scannedCodes.includes(scannedValue)) {
+      setScannedCodes((prev) => {
+        const newScans = [scannedValue, ...prev];
+        return newScans.slice(0, 5); 
+      });
+    }
+  };
+
   return (
-    <Text>barcode</Text>
-  )
-}
+    <View style={styles.container}>
+      <View style={{flex: 1}}>
+        <Camera
+          scanBarcode={true}
+          onReadCode={onBarcodeScan}
+          showFrame={true}
+          laserColor={'red'}
+          frameColor={'blue'}
+          style={{flex: 1}}
+        />
+      </View>
+      <View style={styles.listContainer}>
+        <ThemedText type="subtitle">Last 5 Scanned Barcodes</ThemedText>
+        <FlatList
+          data={scannedCodes}
+          keyExtractor={(item) => item.toString()}
+          renderItem={({ item }) => {
+            return <View style={[styles.scannedItem, { borderColor: color }]}>
+              <ThemedText>{item}</ThemedText>
+            </View>
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  listContainer: {
+    padding: 10,
+    flex: 1
+  },
+  scannedItem: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 8,
+    marginBottom: 10
+  }
+});
